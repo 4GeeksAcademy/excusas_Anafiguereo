8f34e49f6826337427457df2d987b8bb5481ed93
ana <68268688+Angie192020@users.noreply.github.com>
2025-06-01 08:17:07 +0000

    GeneradorExcusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  const elementoAleatorio = (lista) => {
+    let elemento = ""
+    let random = Math.floor( Math.random()* lista.length)
+    elemento = lista [random]
+    console.log (elemento);
+    return elemento 
+  }
+  let excusas = document.querySelector ("#excuse")
+  excusas.innerHTML = elementoAleatorio(who) + " " + elementoAleatorio(action) + " " + elementoAleatorio(what) + " " + elementoAleatorio(when) 
+}
